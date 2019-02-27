df9fee61aa8fb6c0ac97da6db3034c0a301b1764
Florentin Bulot <32537171+FlorentinBulotUoS@users.noreply.github.com>
2019-02-27 14:24:07 +0000

    Rounding
    only one significant digit

--- plantower/plantower.py
+++ plantower/plantower.py
@@ -28,18 +28,18 @@ class PlantowerReading(object):
             an object containing the data
         """
         self.timestamp = datetime.utcnow()
-        self.pm10_cf1 = line[4] * 256 + line[5]
-        self.pm25_cf1 = line[6] * 256 + line[7]
-        self.pm100_cf1 = line[8] * 256 + line[9]
-        self.pm10_std = line[10] * 256 + line[11]
-        self.pm25_std = line[12] * 256 + line[13]
-        self.pm100_std = line[14] * 256 + line[15]
-        self.gr03um = line[16] * 256 + line[17]
-        self.gr05um = line[18] * 256 + line[19]
-        self.gr10um = line[20] * 256 + line[21]
-        self.gr25um = line[22] * 256 + line[23]
-        self.gr50um = line[24] * 256 + line[25]
-        self.gr100um = line[26] * 256 + line[27]
+        self.pm10_cf1 = round(line[4] * 256 + line[5], 1)
+        self.pm25_cf1 = round(line[6] * 256 + line[7], 1)
+        self.pm100_cf1 = round(line[8] * 256 + line[9], 1)
+        self.pm10_std = round(line[10] * 256 + line[11], 1)
+        self.pm25_std = round(line[12] * 256 + line[13], 1)
+        self.pm100_std = round(line[14] * 256 + line[15], 1)
+        self.gr03um = round(line[16] * 256 + line[17], 1)
+        self.gr05um = round(line[18] * 256 + line[19], 1)
+        self.gr10um = round(line[20] * 256 + line[21], 1)
+        self.gr25um = round(line[22] * 256 + line[23], 1)
+        self.gr50um = round(line[24] * 256 + line[25], 1)
+        self.gr100um = round(line[26] * 256 + line[27], 1)
 
     def __str__(self):
         return (
