4eb241f838101078d07016abf53645aadb4e268c
Philip Basford <pjb@ecs.soton.ac.uk>
2020-02-07 16:23:05 +0000

    now tested for active mode

--- test.py
+++ test.py
@@ -2,9 +2,20 @@
 """
     Basic test script to demonstrate active mode of the plantower
 """
+
+from argparse import ArgumentParser
 import plantower
 
+
+PARSER = ArgumentParser(
+        description="Test plantower code in active mode")
+PARSER.add_argument(
+    "port",
+    action="store",
+    help="The serial port to use")
+ARGS = PARSER.parse_args()
+
 #  test code for active mode
-PLANTOWER = plantower.Plantower(port='COM3')
+PLANTOWER = plantower.Plantower(port=ARGS.port)
 print(PLANTOWER.read())
 
