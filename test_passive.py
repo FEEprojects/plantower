ea0eb5403e9e7b26356fd5b8209cf76591e12f0f
Philip Basford <pjb@ecs.soton.ac.uk>
2020-02-07 16:24:55 +0000

    Now tested

--- test_passive.py
+++ test_passive.py
@@ -2,10 +2,21 @@
 """
     Basic test script to demonstrate passive mode of the plantower
 """
+from argparse import ArgumentParser
 import time
 import plantower
 
-PLANTOWER = plantower.Plantower(port='COM3') # create the object
+
+PARSER = ArgumentParser(
+        description="Test plantower code in passive mode")
+PARSER.add_argument(
+    "port",
+    action="store",
+    help="The serial port to use")
+ARGS = PARSER.parse_args()
+
+
+PLANTOWER = plantower.Plantower(port=ARGS.port) # create the object
 
 PLANTOWER.mode_change(plantower.PMS_PASSIVE_MODE) #change into passive mode
 time.sleep(5) #give the sensor a chance to settle
